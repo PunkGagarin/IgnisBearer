041afe947c12c72fe3464696b27a7ff8aee39b2d
madgine <marinabelogaz@gmail.com>
2025-11-26 22:44:58 +0300

    fix merge;

--- Assets/_Project/Scripts/Infrastructure/GameStates/States/BootstrapState.cs
+++ Assets/_Project/Scripts/Infrastructure/GameStates/States/BootstrapState.cs
@@ -1,6 +1,3 @@
-using Project.Scripts.Audio;
-using Project.Scripts.Infrastructure.GameStates;
-using Project.Scripts.Infrastructure.GameStates.States;
 using _Project.Scripts.Audio.Domain;
 using Zenject;
 
@@ -8,18 +5,16 @@ namespace _Project.Scripts.Infrastructure.GameStates.States
 {
     public class BootstrapState : IState, IGameState
     {
-
         [Inject] private GameStateMachine _gameStateMachine;
         [Inject] private AudioService _audioService;
 
         public void Enter()
         {
-            
             // show curtain
             // some resource loading
             // asset provider loading etc
             // init all project context systems
-            
+
             _audioService.Init();
             _gameStateMachine.Enter<MainMenuState>();
         }
