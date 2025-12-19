22da68199b49dd087b9ab9abb717ca1f5d87a0f8
RomanT <r.tarasyuta@chillgaming.net>
2025-12-20 05:01:50 +0700

    lantern spawn and create lights

---
+++ Assets/_Project/Scripts/Gameplay/Buildings/Lanterns/LanternLightCreator.cs
@@ -0,0 +1,75 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace _Project.Scripts.Gameplay.Buildings.Lanterns
+{
+
+    [RequireComponent(typeof(ResourceProducer))]
+    public class LanternLightCreator : MonoBehaviour
+    {
+        [field: SerializeField]
+        private LightResource LightPrefab { get; set; }
+
+        [field: SerializeField]
+        private Transform SpawnPoint { get; set; }
+
+        [field: SerializeField]
+        public float DropSpehereRadius { get; private set; } = 3f;
+
+        [field: SerializeField]
+        public float DropAnimationTime { get; private set; } = 1f;
+
+        [field: SerializeField]
+        public float YBuffer { get; private set; } = 3f;
+        
+        [field: SerializeField]
+        public AnimationCurve AnimaCurve { get; private set; }
+
+        //create object pool
+        private ResourceProducer _resourceProducer;
+
+        private void Awake()
+        {
+            _resourceProducer = GetComponent<ResourceProducer>();
+        }
+
+        private void Start()
+        {
+            _resourceProducer.OnProduced += CreateLight;
+        }
+
+        private void OnDestroy()
+        {
+            _resourceProducer.OnProduced -= CreateLight;
+        }
+
+        private void CreateLight(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var lightResource = Instantiate(LightPrefab, SpawnPoint.position, Quaternion.identity, transform);
+
+                Vector3 endPosition = FindRandomPositionToDropResource();
+                lightResource.SetFinalPosition(endPosition);
+
+                lightResource.transform
+                    .DOMove(endPosition, DropAnimationTime)
+                    .SetEase(AnimaCurve)
+                    .ToUniTask(cancellationToken: destroyCancellationToken);
+            }
+        }
+
+        private Vector3 FindRandomPositionToDropResource()
+        {
+            var spawnPointPosition = SpawnPoint.position;
+            var onUnitSphere = Random.onUnitSphere * DropSpehereRadius;
+            Vector3 finalPosition = spawnPointPosition + onUnitSphere - new Vector3(0, YBuffer, 0);
+            finalPosition.z = 0;
+
+            return finalPosition;
+        }
+    }
+
+}
---
+++ Assets/_Project/Scripts/Gameplay/Buildings/Lanterns/LightResource.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Gameplay.Buildings.Lanterns
+{
+    public class LightResource : MonoBehaviour
+    {
+        public Vector3 FinalPosition { get; private set; }
+
+
+        public void SetFinalPosition(Vector3 pos)
+        {
+            FinalPosition = pos;
+        }
+
+    }
+}
