09904361445f4fbc4eae69892adb6370ebf048c3
madgine <marinabelogaz@gmail.com>
2025-12-22 21:28:55 +0300

    hiding unit on adding to fate gen;

---
+++ Assets/_Project/Scripts/Gameplay/Buildings/Building/FateGenerator/FateGeneratorUnitIncreaser.cs
@@ -0,0 +1,15 @@
+﻿using _Project.Scripts.Gameplay.Units;
+using UnityEngine;
+
+namespace _Project.Scripts.Gameplay.Buildings.FateGenerator
+{
+    [RequireComponent(typeof(IWorkers))]
+    public class FateGeneratorUnitIncreaser : BuildingUnitIncreaser
+    {
+        protected override void OnUnitAdded(Unit unit)
+        {
+            unit.StateMachine.Enter<UnitMoveToWithNext, DisableState, Vector3>(transform.position);
+        }
+
+    }
+}
