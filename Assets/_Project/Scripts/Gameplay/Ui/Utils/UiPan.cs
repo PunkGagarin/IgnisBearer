7852449686fa5698ee5d6529d6ea4a89fc81c989
punkgagarin <punkgagarin@gmail.com>
2026-01-06 15:07:06 +0700

    ui zoom and pan added for tree

---
+++ Assets/_Project/Scripts/Gameplay/Ui/Utils/UiPan.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _Project.Scripts.Gameplay.Ui.Utils
+{
+    public class UiPan : MonoBehaviour, IDragHandler
+    {
+        public RectTransform treeContainer;
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            treeContainer.anchoredPosition += eventData.delta;
+        }
+    }
+}
