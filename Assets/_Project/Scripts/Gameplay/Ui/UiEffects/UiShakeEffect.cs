c2653632b04ee8d3ec4aa71df3ad778dcc3623a8
madgine <marinabelogaz@gmail.com>
2026-01-13 22:02:59 +0300

    fixed shake ui;

--- Assets/_Project/Scripts/Gameplay/Ui/UiEffects/UiShakeEffect.cs
+++ Assets/_Project/Scripts/Gameplay/Ui/UiEffects/UiShakeEffect.cs
@@ -9,19 +9,20 @@ namespace _Project.Scripts.Gameplay.Ui.UiEffects
     {
         [Inject] private readonly UiSettings _settings;
         private RectTransform _rectTransform;
-        private Vector2 _originalAnchoredPos;
-
 
         private void Awake()
         {
             _rectTransform = GetComponent<RectTransform>();
-            _originalAnchoredPos = _rectTransform.anchoredPosition;
         }
 
         public void Shake()
         {
-            _rectTransform.DOShakeAnchorPos(_settings.ShakeDuration, _settings.ShakeStrength, _settings.ShakeVibrato, _settings.ShakeRandomness)
-                .OnComplete(() => _rectTransform.anchoredPosition = _originalAnchoredPos);
+            _rectTransform.DOShakePosition(
+                _settings.ShakeDuration,
+                _settings.ShakeStrength,
+                _settings.ShakeVibrato,
+                _settings.ShakeRandomness
+            );
         }
     }
 }
