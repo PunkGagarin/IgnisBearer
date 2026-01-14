cd4df761872bd71bc55874fd35477ba8e523c475
punkgagarin <punkgagarin@gmail.com>
2026-01-14 10:02:59 +0700

    Building settings added + few fixes

---
+++ Assets/_Project/Scripts/Gameplay/Buildings/BuildingsSlots/BuildingSlotEnabler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using _Project.Scripts.Gameplay.Tutorial;
+using Zenject;
+
+namespace _Project.Scripts.Gameplay.Buildings.BuildingsSlots
+{
+    public class BuildingSlotEnabler : IDisposable
+    {
+        private int _currentRes;
+        private int _targetRes;
+
+        [Inject] private BuildingsService _buildingsService;
+        [Inject] private BuildingSlotsService _buildingSlotsService;
+        [Inject] private TutorialSettings _tutorialSettings;
+
+        public void Init()
+        {
+            var church = _buildingsService.GetChurch();
+            
+            //todo: change to BarrierService or smth like that which should be indepandable from church maybe??
+            var lightResourceStorage = church.GetComponent<IResourceStorage>();
+            lightResourceStorage.OnAmountIncreased += Iterate;
+
+            _targetRes = _tutorialSettings.LightCountForChurch;
+        }
+
+        private void Iterate((int amountIncreased, int newAmount, int maxAmount) valueTuple)
+        {
+            _currentRes++;
+            if (_currentRes >= _targetRes)
+                _buildingSlotsService.EnableButtonForSlots();
+        }
+
+
+        public void Dispose()
+        {
+            var church = _buildingsService.GetChurch();
+            var lightResourceStorage = church.GetComponent<IResourceStorage>();
+            lightResourceStorage.OnAmountIncreased -= Iterate;
+        }
+    }
+}
