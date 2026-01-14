cb0e1bd51468ee174177b118c7c6455b59ab0543
punkgagarin <punkgagarin@gmail.com>
2026-01-15 03:27:06 +0700

    only one popup at a time

---
+++ Assets/_Project/Scripts/Gameplay/Buildings/BuildingsSlots/PopupController.cs
@@ -0,0 +1,15 @@
+﻿using _Project.Scripts.Gameplay.Ui.UiEffects;
+
+namespace _Project.Scripts.Gameplay.Buildings.BuildingsSlots
+{
+    public class PopupController
+    {
+        private UiPopupDisplayer _uiPopupDisplayer;
+
+        public void Register(UiPopupDisplayer uiPopupDisplayer)
+        {
+            _uiPopupDisplayer?.AnimateAndHide();
+            _uiPopupDisplayer = uiPopupDisplayer;
+        }
+    }
+}
