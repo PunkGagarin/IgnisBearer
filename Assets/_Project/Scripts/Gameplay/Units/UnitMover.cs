0b10d8450649104e29cb73d38954e4301ab2a4f3
madgine <marinabelogaz@gmail.com>
2026-01-21 21:02:43 +0300

    imported test unit art & anim;

--- Assets/_Project/Scripts/Gameplay/Units/UnitMover.cs
+++ Assets/_Project/Scripts/Gameplay/Units/UnitMover.cs
@@ -8,37 +8,63 @@ namespace _Project.Scripts.Gameplay.Units
 {
     public class UnitMover : MonoBehaviour
     {
+        private static readonly int IsMoving = Animator.StringToHash("isMoving");
+        private static readonly int LastX = Animator.StringToHash("LastX");
+        private static readonly int LastY = Animator.StringToHash("LastY");
+        private static readonly int CurX = Animator.StringToHash("CurX");
+        private static readonly int CurY = Animator.StringToHash("CurY");
+        
+        [field:SerializeField] private Animator _animator;
+        [SerializeField] private Vector2 lightOffsetRight = new(1f, 0.0f);
+        [SerializeField] private Vector2 lightOffsetLeft  = new(-1f, 0.0f);
+        
         private Unit _unit;
-
-        private Transform _transform;
 
         public Action<Unit> OnReach = delegate { };
 
         private void Awake()
         {
             _unit = GetComponent<Unit>();
-            _transform = GetComponent<Transform>();
         }
-
-        private void FlipIfNeeded(Vector3 destination)
+        
+        private void UpdateLightDirection(Vector2 dir)
         {
-            if (destination.x > transform.position.x)
-                _transform.localScale = new Vector2(1, _transform.localScale.y);
-            else
-                _transform.localScale = new Vector2(-1, _transform.localScale.y);
-        }
+            dir.Normalize(); 
+            // todo need to recheck after import final art
 
+            if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+            {
+                _unit.Light2D.transform.localPosition = dir.x > 0 ? lightOffsetRight : lightOffsetLeft;
+            }
+        }
 
         public UniTask MoveTo(Vector3 destination, MoveType moveType = MoveType.Run, CancellationToken cancellationToken = default)
         {
             //todo: если уничтожаем юнита он продолжает двигаться, надо залинковать внешний токен с монобехом
             var speed = GetSpeedByType(moveType);
-            FlipIfNeeded(destination);
+            
+            
+            _animator.SetBool(IsMoving, true);
+            var dir = (destination - transform.position).normalized;
+            var x = dir.x;
+            var y = dir.y;
+            UpdateLightDirection(dir);
 
             var task = transform.DOMove(destination, speed)
                 .SetSpeedBased()
                 .SetEase(Ease.Linear)
-                .OnComplete(() => OnReach.Invoke(_unit))
+                .OnUpdate(() =>
+                {
+                    _animator.SetFloat(CurX, x);
+                    _animator.SetFloat(CurY, y);
+                })
+                .OnComplete(() =>
+                {
+                    _animator.SetFloat(LastX, x);
+                    _animator.SetFloat(LastY, y);
+                    _animator.SetBool(IsMoving, false);
+                    OnReach.Invoke(_unit);
+                })
                 .ToUniTask(cancellationToken: cancellationToken);
 
             return task;
