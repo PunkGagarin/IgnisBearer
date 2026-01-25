dc4a20b21a8ddcb93a515cab0669faeb93b1c3c3
punkgagarin <punkgagarin@gmail.com>
2026-01-25 19:05:22 +0700

    if resource was clicked it will not destroyed

--- Assets/_Project/Scripts/Gameplay/Buildings/Lanterns/LightResource.cs
+++ Assets/_Project/Scripts/Gameplay/Buildings/Lanterns/LightResource.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 namespace _Project.Scripts.Gameplay.Buildings.Lanterns
 {
+    [RequireComponent(typeof(AutodestroyTimer))]
     public class LightResource : ClickableView<LightResource>
     {
         public Vector3 FinalPosition { get; private set; }
         
         public Action<LightResource> OnHarvested = delegate { };
 
+        private AutodestroyTimer _timer;
 
+        public override void Awake()
+        {
+            base.Awake();
+            _timer = GetComponent<AutodestroyTimer>();
+        }
+        
+        
         public void SetFinalPosition(Vector3 pos)
         {
             FinalPosition = pos;
@@ -24,6 +33,7 @@ namespace _Project.Scripts.Gameplay.Buildings.Lanterns
         public void SetBusy()
         {
             DisableInteract();
+            _timer.StopTimer();
         }
     }
 }
